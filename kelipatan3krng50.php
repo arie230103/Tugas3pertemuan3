caea7b6378c6626452939b33698388b527d6aa3d
Arie Satria <92197177+arie230103@users.noreply.github.com>
2022-10-06 00:18:06 +0700

    Add files via upload

---
+++ kelipatan3krng50.php
@@ -0,0 +1,10 @@
+
+<?php
+ //Buat kelipatan 3.php dan looping yang menghasilkan bilangan kelipatan 3 yang kurang dari 50
+ echo "<h2>Kelipatan 3 kurang dari 50</h2>";
+ $bil = 3;
+ while($bil<50){
+  echo "Bilangan : $bil <br/>";
+  $bil=$bil+3;
+ }
+?>
